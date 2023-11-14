7b92f7666201f17bd7afd04644927965321c6a6b
lightSource0713 <kingstone0713@gmail.com>
2023-11-14 16:01:55 -0300

    initial

---
+++ test.ts
@@ -0,0 +1,12 @@
+// exemplo de contextos em javascript
+{
+    const numero = 2
+}
+// contexto x e y em que cada contexto tem uma variavel diferente 
+{
+    const numero = 1
+}
+// funcao anonima
+() => {
+    const numero = 4
+}
